245f145f0a42cac65c8d37357d87be653e867823
unknown <chanchal kolekar>
2023-02-10 04:04:59 +0530

    I have done Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+       // obj.name = "vartika";
+       // System.out.println(obj.name);
+        obj.setName("vartika");
+        System.out.println(obj.getName());
+    }
   
 }
